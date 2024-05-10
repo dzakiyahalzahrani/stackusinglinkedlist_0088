a41449d56fb9f6f1c64e9c37b5610eade0d38a7d
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-05-10 14:00:07 +0700

    cout Popped value

--- stackusinglinkedlist_0088/stackusinglinkedlist_0088.cpp
+++ stackusinglinkedlist_0088/stackusinglinkedlist_0088.cpp
@@ -31,7 +31,10 @@ public:
 
 	void pop() {
 		if (isEmpty()) {
-			cout << "Stack is Empty." << endl;
+			cout << "Stack is empty." << endl;
 		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
 	}
+	
 };
